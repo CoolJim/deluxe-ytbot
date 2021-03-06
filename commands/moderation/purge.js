e8c9a9576c284781d827f6b9abcf068c146c0f84
Jim <jimke2000@gmail.com>
2021-03-06 20:07:21 +1100

    made it work

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -11,16 +11,16 @@ module.exports = {
       return message.channel.send(
         `${message.author}, please specify how many messages to delete.`
       );
-    var del = parseInt(args[0]);
-    if (Number.isNaN(del))
+    const del = parseInt(args[0]);
+    if (isNaN(del))
       return message.channel.send(
         `${message.author}, specify a valid number`
       );
-    if (!del > 0 || !del < 99) return message.channel.send(`Number needs to be <99 and >0`);
+    if (del < 2 || del > 100) return message.channel.send(`Number needs to be <100 and >1 (2-99)`);
     message.channel.bulkDelete(del, true).catch((err) => {
       console.error(err);
       message.channel.send(
-        `Could not prune messages, remember I cannot delete messages older than 2 weeks, and can only delete <99 messages.`
+        `Could not prune messages, remember I cannot delete messages older than 2 weeks, and can only delete up to 99 messages.`
       );
     });
   },
