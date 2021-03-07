b3bed853d1deb8049539a6c97727b373289dad43
Jim <jimke2000@gmail.com>
2021-03-07 19:43:40 +1100

    Made balance work

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -1,28 +1,26 @@
 const db = require("quick.db");
-const discord = require("discord.js");
-const index = require('../../index.js')
+const Discord = require("discord.js");
+
 module.exports = {
   name: "balance",
   aliases: ["bal", "wallet", "cash"],
   usage: "[mention]",
   description: "Check your wallet balance.",
-  async execute(message, bot, args) {
-    console.log("log`d");
+  execute(message, bot, args) {
+
     const user = message.mentions.users.first() || message.author;
-    console.log('hiiiii');
-    console.log('deth');
-    let money = await index.economy.get(`${user.id}_cash`);
-    console.log('ho chi min');
-    let bank = await index.economy.get(`${user.id}_bank`);
-    console.log('i c u p')
+
+    let money = db.get(`${user.id}_cash`);
+
+    let bank = db.get(`${user.id}_bank`);
+
     if (!money || money == null) money = 0;
     if (!bank || bank == null) bank = 0;
     const balance = new Discord.MessageEmbed()
       .setColor("RANDOM")
-      .setTitle(`${user}'s balance`)
+      .setTitle(`${user.username}'s balance`)
       .setDescription(`Wallet: ${money}\nBank: ${bank}`)
       .setTimestamp();
-    console.log("log2");
     message.channel.send(balance);
   },
 };
