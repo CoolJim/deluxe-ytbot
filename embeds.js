bf148dfffaa903f3c52d0305321244249b0cab71
Jim <jimke2000@gmail.com>
2021-03-10 18:26:45 +1100

    embeds file, collate default error/other embeds here

---
+++ embeds.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+// Styles
+const error = `An error was encountered executing that command`;
+const errorImg = './assets/img/error.png';
+const link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+const guildOnly = new Discord.MessageEmbed()
+  .setTitle(error)
+  .setDescription(`This command can only be executed within a server, and cannot be executed directly!`)
+  .setThumbnail(errorImg)
+  .setURL(link)
+  .setTimestamp()
+  .setColor('RANDOM')
+  .setFooter('Never gonna give you up, never gonna let you down, never...');
+
+// Exports
+module.exports = (guildOnly);
