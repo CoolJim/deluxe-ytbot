d59ba1f74302e2ccdfa2e39a5845c7d844199f8c
Jim <jimke2000@gmail.com>
2021-03-12 19:40:58 +1100

    inventory check

---
+++ commands/economy/inventory.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+const embeds = require('../../embeds.js');
+
+module.exports = {
+  name: 'inventory',
+  aliases: ['inv', 'inventories'],
+  description: 'Check your inventory',
+  args: false,
+  cooldown: 5,
+  category: 'Economy',
+  async execute(message, args, bot) {
+    let user = message.author;
+    let items = db.get('user.id.items') || `${user.username} has no items`;
+
+    const inv = new Discord.MessageEmbed()
+      .setTitle(`${user.username}'s items'`)
+      .setColor('RANDOM')
+      .addField("Inventory", items);
+    message.channel.send(inv);
+
+
+
+  }
+}
