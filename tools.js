46fedcdeab74809b26a15ee42e2c837980cecfb4
Jim <jimke2000@gmail.com>
2021-03-24 19:59:43 +1100

    add chalk, error reporting

--- tools.js
+++ tools.js
@@ -1,18 +1,9 @@
 // Imports
-const index = require("./index.js");
+const chalk = require('chalk')
 // Exports
 module.exports = {
-  parseMention: function (mention) {
-    // The id is the first and only match found by the RegEx.
-    const matches = mention.match(/^<@!?(\d+)>$/);
-
-    // If supplied variable was not a mention, matches will be null instead of an array.
-    if (!matches) return;
-
-    // However the first element in the matches array will be the entire mention, not just the ID,
-    // so use index 1.
-    const id = matches[1];
-
-    return index.bot.users.cache.get(id);
-  },
-};
+  errorReport(e) {
+    log(chalk.bgRed.bold(`Caught error`));
+    log(chalk.red(e));
+  }
+}
