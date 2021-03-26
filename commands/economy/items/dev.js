4997ae8e3950dd4dd9986f7b47f5c67054b5ad39
Jim <jimke2000@gmail.com>
2021-03-26 16:33:57 +1100

    Make dev item restricted

--- commands/economy/items/dev.js
+++ commands/economy/items/dev.js
@@ -3,8 +3,8 @@ module.exports = {
     description: 'Some sick developer clothes. Welcome to the team my guy!',
     cost: Infinity,
     sell: 69420,
-    collectable: false,
-    restricted: true,
+    collectable: trye,
+        restricted: true,
     execute(message, args, db) {
         return message.channel.send(`${message.author}, welcome to the devteam`);
     }
