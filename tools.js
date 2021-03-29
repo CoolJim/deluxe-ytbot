bfecee53352dc41bcbcd790b379dedc89ae1f3b8
Jim <43405050+CoolJim@users.noreply.github.com>
2021-03-29 17:13:11 +1100

    new functions

--- tools.js
+++ tools.js
@@ -8,4 +8,13 @@ module.exports = {
 		console.log(chalk.bgRed.bold(`Caught error`));
 		console.log(chalk.red(e));
 	},
+	rand(low, high) {
+		return Math.floor(Math.random() * low) + high;
+	},
+	parseMention(client, mention) {
+		const matches = mention.match(/^<@!?(\d+)>$/);
+		if (!matches) return null;
+		const id = matches[1];
+		return client.users.cache.get(id);
+	},
 };
